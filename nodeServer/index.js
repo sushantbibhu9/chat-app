1921f1a9b1f2b6a19ad6799ac243dbbbd268aa34
Sushant Singh <sushantbibhu@gmail.com>
2023-07-03 18:16:58 +0530

    add-io.on

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,3 +1,18 @@
+const { Socket } = require('socket.io');
+
 //node server 
 const io = require('socket.io')(8000)
 
+const users = {};
+
+io.on('connection', socket =>{
+    socket.on('new-user-joined', name =>{
+        console.log("New user", name);
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    socket.on('send', message =>{
+        socket.broadcast.emit('receive', {message, name: user[socket.id]})
+    });
+})
